c33df7f6127b84634689fc84bcadeb190eddbf32
unknown <Nate@Intel1.(none)>
2011-11-09 18:41:03 -0800

    Every day I'm bug fixin

--- src/graphics/model/ModelBuilder.java
+++ src/graphics/model/ModelBuilder.java
@@ -1,70 +1,69 @@
 package graphics.model;
 
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
+import javax.vecmath.Point2f;
 import javax.vecmath.Vector3f;
 
-import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
-
-import com.bulletphysics.collision.shapes.BvhTriangleMeshShape;
-import com.bulletphysics.collision.shapes.CollisionShape;
-import com.bulletphysics.collision.shapes.IndexedMesh;
-import com.bulletphysics.collision.shapes.TriangleIndexVertexArray;
 
 public class ModelBuilder {
 	/** the name of the model */
 	public String name;
 
-	/** the indexed mesh, used to create the ship for JBullet */
-	public IndexedMesh mesh;
-	private ByteBuffer triangleIndexBase;
-	private ByteBuffer vertexBase;
-
+	// the vertices of the model
 	private ArrayList<Vector3f> vertices;
+
+	// the normals of the model
 	private ArrayList<Vector3f> normals;
-	private ArrayList<Integer> triangleIndices;
-	private ArrayList<Integer> normalIndices;
+
+	// the texture coordinates of the model
+	private ArrayList<Point2f> textureCoords;
+
+	// which vertices to call
+	private ArrayList<int[]> vertexIndices;
+
+	// which normals to call
+	private ArrayList<int[]> normalIndices;
+
+	// which texture coordinates to call
+	private ArrayList<int[]> textureIndices;
 
 	/** for rendering the model */
 	private int callList;
 
 	public ModelBuilder() {
-		mesh = new IndexedMesh();
-
-		// each float is 4 bytes
-		mesh.triangleIndexStride = 4;
-		// an int is also 4 bytes
-		mesh.vertexStride = 4;
-		
 		vertices = new ArrayList<Vector3f>();
 		normals = new ArrayList<Vector3f>();
-		triangleIndices = new ArrayList<Integer>();
-		normalIndices = new ArrayList<Integer>();
-		
+		textureCoords = new ArrayList<Point2f>();
+
+		vertexIndices = new ArrayList<int[]>();
+		normalIndices = new ArrayList<int[]>();
+		textureIndices = new ArrayList<int[]>();
 	}
-	
+
 	public void addVertex(Vector3f vertex) {
 		vertices.add(vertex);
 	}
 
 	public void addVertexIndices(int[] indices) {
 		if (indices.length == 3) {
-			triangleIndices.add(indices[0]);
-			triangleIndices.add(indices[1]);
-			triangleIndices.add(indices[2]);
+			vertexIndices.add(indices);
 		} else if (indices.length == 4) {
-			//first triangle from the quad
-			triangleIndices.add(indices[0]);
-			triangleIndices.add(indices[1]);
-			triangleIndices.add(indices[2]);
-			
+			// first triangle from the quad
+			int[] tri1 = new int[3];
+			tri1[0] = indices[0];
+			tri1[1] = indices[1];
+			tri1[2] = indices[2];
+			vertexIndices.add(tri1);
+
 			// second triangle from the quad
-			triangleIndices.add(indices[2]);
-			triangleIndices.add(indices[3]);
-			triangleIndices.add(indices[0]);
-		} else{
+			int[] tri2 = new int[3];
+			tri2[0] = indices[2];
+			tri2[1] = indices[3];
+			tri2[2] = indices[0];
+			vertexIndices.add(tri2);
+		} else {
 			System.out.println("Vertex indices not a triangle or a quad!");
 		}
 	}
@@ -75,178 +74,145 @@ public class ModelBuilder {
 
 	public void addNormalIndices(int[] indices) {
 		if (indices.length == 3) {
-			normalIndices.add(indices[0]);
-			normalIndices.add(indices[1]);
-			normalIndices.add(indices[2]);
-		} else if(indices.length == 4){
-			normalIndices.add(indices[0]);
-			normalIndices.add(indices[1]);
-			normalIndices.add(indices[2]);
-			
-			normalIndices.add(indices[2]);
-			normalIndices.add(indices[3]);
-			normalIndices.add(indices[0]);
-		} else{
+			normalIndices.add(indices);
+		} else if (indices.length == 4) {
+			// first triangle from the quad
+			int[] tri1 = new int[3];
+			tri1[0] = indices[0];
+			tri1[1] = indices[1];
+			tri1[2] = indices[2];
+			normalIndices.add(tri1);
+
+			// second triangle from the quad
+			int[] tri2 = new int[3];
+			tri2[0] = indices[2];
+			tri2[1] = indices[3];
+			tri2[2] = indices[0];
+			normalIndices.add(tri2);
+		} else {
 			System.out.println("Normal indices not a triangle or a quad!");
 		}
 	}
-	
+
+	public void addTetxureIndices(int[] indices) {
+		if (indices.length == 3) {
+			textureIndices.add(indices);
+		} else if (indices.length == 4) {
+			// first triangle from the quad
+			int[] tri1 = new int[3];
+			tri1[0] = indices[0];
+			tri1[1] = indices[1];
+			tri1[2] = indices[2];
+			textureIndices.add(tri1);
+
+			// second triangle from the quad
+			int[] tri2 = new int[3];
+			tri2[0] = indices[2];
+			tri2[1] = indices[3];
+			tri2[2] = indices[0];
+			textureIndices.add(tri2);
+		} else {
+			System.out.println("Texture indices not a triangle or a quad!");
+		}
+	}
+
 	/**
 	 * This method should be called after all the vertices, faces, and normals
 	 * and their respective indices have been added.
 	 */
 	public Model makeModel(float scale) {
-		mesh.numTriangles = triangleIndices.size() / 3;
-		mesh.numVertices = vertices.size();
-		
+		checkForCompleteness();
+
+		buildCallList();
+
+		return null;
+	}
+
+	private void buildCallList() {
 		callList = GL11.glGenLists(1);
-		
-		GL11.glNewList(callList, GL11.GL_COMPILE);{
-			GL11.glBegin(GL11.GL_TRIANGLES);{
-				for(int i = 0; i < vertices.size(); i++){
-					
+
+		GL11.glNewList(callList, GL11.GL_COMPILE);
+		{
+			GL11.glBegin(GL11.GL_TRIANGLES);
+			{
+				for (int i = 0; i < vertexIndices.size(); i++) {
+					int[] verts = vertexIndices.get(i);
+					Vector3f vertex1 = vertices.get(verts[0]);
+					Vector3f vertex2 = vertices.get(verts[1]);
+					Vector3f vertex3 = vertices.get(verts[2]);
+
+					// only grab normal indices if they exist
+					int[] norms = null;
+					Vector3f normal1 = null;
+					Vector3f normal2 = null;
+					Vector3f normal3 = null;
+					if (normalIndices.size() != 0) {
+						norms = normalIndices.get(i);
+						normal1 = normals.get(norms[0]);
+						normal2 = normals.get(norms[1]);
+						normal3 = normals.get(norms[2]);
+					}
+
+					// only grab texture indices if they exist
+					int[] texts = null;
+					Point2f texture1 = null;
+					Point2f texture2 = null;
+					Point2f texture3 = null;
+					if (textureIndices.size() != 0) {
+						texts = textureIndices.get(i);
+						texture1 = textureCoords.get(texts[0]);
+						texture2 = textureCoords.get(texts[1]);
+						texture3 = textureCoords.get(texts[2]);
+					}
+
+					GL11.glVertex3f(vertex1.x, vertex1.y, vertex1.z);
+					if (normal1 != null)
+						GL11.glNormal3f(normal1.x, normal1.y, normal1.z);
+					if (texture1 != null)
+						GL11.glTexCoord2f(texture1.x, texture1.y);
+
+					GL11.glVertex3f(vertex2.x, vertex2.y, vertex2.z);
+					if (normal2 != null)
+						GL11.glNormal3f(normal2.x, normal2.y, normal2.z);
+					if (texture2 != null)
+						GL11.glTexCoord2f(texture2.x, texture2.y);
+
+					GL11.glVertex3f(vertex3.x, vertex3.y, vertex3.z);
+					if (normal3 != null)
+						GL11.glNormal3f(normal3.x, normal3.y, normal3.z);
+					if (texture3 != null)
+						GL11.glTexCoord2f(texture3.x, texture3.y);
 				}
-			} GL11.glEnd();
-		} GL11.glEndList();
-		
+			}
+			GL11.glEnd();
+		}
+		GL11.glEndList();
 	}
 
 	/**
-	 * This method should be called after all the vertices, faces, and normals
-	 * and their respective indices have been added.
+	 * Makes sure that there are at least some vertices. If there are normals it
+	 * makes sure that there's one for every vertex. Does the same for texture
+	 * coordinates.
 	 */
-	public Model makeModelOld(float scale) {
-		try {
-			// the indices are stored as singular ints, in groups of threes
-			// (three per triangle)
-			mesh.numTriangles = triangleIndices.size() / 3;
-			// vertices are stored as Vector3f objects, so the size of the list
-			// is the number of vertices
-			mesh.numVertices = vertices.size();
-
-			/*
-			 * Vertices are stored in the vertices ArrayList as Vector3f
-			 * objects. This means that for every index in 'vertices', we have
-			 * three floats representing the vertex. So we multiply the size by
-			 * three, as we only want single floats in our ByteBuffer. We
-			 * multiply that by the vertexStride (number of bytes per float; 4)
-			 * to get the total number of bytes for the buffer.
-			 */
-			vertexBase = BufferUtils.createByteBuffer(vertices.size() * 3
-					* mesh.vertexStride);
-			for (Vector3f vect : vertices) {
-				vertexBase.putFloat(vect.x * scale);
-				vertexBase.putFloat(vect.y * scale);
-				vertexBase.putFloat(vect.z * scale);
-			}
-
-			/*
-			 * Indices are stored in the faceIndices ArrayList as ints. So we
-			 * want number of indices * stride (number of bytes per int; 4)
-			 * bytes for the buffer.
-			 */
-			triangleIndexBase = BufferUtils.createByteBuffer(triangleIndices
-					.size() * mesh.triangleIndexStride);
-			for (int index : triangleIndices) {
-				triangleIndexBase.putInt(index);
-			}
-
-			// set the mesh's bases
-			mesh.vertexBase = vertexBase;
-			mesh.triangleIndexBase = triangleIndexBase;
-
-			// create a TriangleIndexVertxArray and the collision shape
-			TriangleIndexVertexArray triangleArray = new TriangleIndexVertexArray();
-			triangleArray.addIndexedMesh(mesh);
-			//CollisionShape collisionShape = new BvhTriangleMeshShape(
-			//		triangleArray, true);
-
-			/* BEGIN CALL LIST BUILDING */
-			callList = GL11.glGenLists(1);
-
-			if (triangleIndices.size() != normalIndices.size()
-					&& normalIndices.size() > 0)
-				System.out
-						.println("triangleIndices size is not equal to normalIndices size!!!");
-
-			System.out.println("SIZE***" + vertices.size() + " " + normals.size() + " " + triangleIndices.size() + " " + normalIndices.size());
-			GL11.glNewList(callList, GL11.GL_COMPILE);
-			{
-				GL11.glBegin(GL11.GL_TRIANGLES);
-				{
-					for (int i = 0; i < triangleIndices.size(); i++) {
-						// grab the three vertices
-						int v1 = triangleIndices.get(i);
-
-						// FIXME this needs to only grab/use normals if they
-						// exist, otherwise is just does the vertices
-						int n1 = normalIndices.get(i);
-
-						// grab the normals
-						Vector3f normal1 = normals.get(n1);
-
-						// grab the vertices
-						Vector3f vertex1 = vertices.get(v1);
-
-						// draw the first normal/vertex
-						GL11.glNormal3f(normal1.x * scale, normal1.y * scale,
-								normal1.z * scale);
-						GL11.glVertex3f(vertex1.x * scale, vertex1.y * scale,
-								vertex1.z * scale);
-						
-						/*
-						// grab the three vertices
-						int v1 = triangleIndices.get(i);
-						int v2 = triangleIndices.get(i + 1);
-						int v3 = triangleIndices.get(i + 2);
-
-						// FIXME this needs to only grab/use normals if they
-						// exist, otherwise is just does the vertices
-						int n1 = normalIndices.get(i);
-						int n2 = normalIndices.get(i + 1);
-						int n3 = normalIndices.get(i + 2);
-
-						// grab the normals
-						Vector3f normal1 = normals.get(n1);
-						Vector3f normal2 = normals.get(n2);
-						Vector3f normal3 = normals.get(n3);
-
-						// grab the vertices
-						Vector3f vertex1 = vertices.get(v1);
-						Vector3f vertex2 = vertices.get(v2);
-						Vector3f vertex3 = vertices.get(v3);
-
-						// draw the first normal/vertex
-						GL11.glNormal3f(normal1.x * scale, normal1.y * scale,
-								normal1.z * scale);
-						GL11.glVertex3f(vertex1.x * scale, vertex1.y * scale,
-								vertex1.z * scale);
-
-						// draw the second normal/vertex
-						GL11.glNormal3f(normal2.x * scale, normal2.y * scale,
-								normal2.z * scale);
-						GL11.glVertex3f(vertex2.x * scale, vertex2.y * scale,
-								vertex2.z * scale);
-
-						// draw the third normal/vertex
-						GL11.glNormal3f(normal3.x * scale, normal3.y * scale,
-								normal3.z * scale);
-						GL11.glVertex3f(vertex3.x * scale, vertex3.y * scale,
-								vertex3.z * scale);
-								*/
-					}
-				}
-				GL11.glEnd();
-			}
-			GL11.glEndList();
-
-			/* END CALL LIST BUILDING */
-
-			return new Model(null, callList);
-		} catch (Exception e) {
-			e.printStackTrace();
+	private void checkForCompleteness() {
+		// FIXME This should probably throw exceptions (maybe create a
+		// ModelBuilderException?)
+		if (vertices.size() == 0 || vertexIndices.size() == 0) {
+			System.out
+					.println("There aren't any vertices! You sure you want to make this model?");
 		}
-		return null;
+
+		if (normalIndices.size() != 0) {
+			if (normalIndices.size() != vertexIndices.size())
+				System.out
+						.println("Different amount of normal and vertex indices!");
+		}
+
+		if (textureIndices.size() != 0) {
+			if (textureIndices.size() != vertexIndices.size())
+				System.out
+						.println("Different amount of texture indices and vertex indices!");
+		}
 	}
 
 	public int getCallList() {
