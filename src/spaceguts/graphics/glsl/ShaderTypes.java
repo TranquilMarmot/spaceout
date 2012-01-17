b82340deede56a879af3772b69349f3a6834f90c
TranquilMarmot <nate272@gmail.com>
2012-01-17 08:48:14 -0800

    Fixed ShaderTypes enum

--- src/spaceguts/graphics/glsl/ShaderTypes.java
+++ src/spaceguts/graphics/glsl/ShaderTypes.java
@@ -1,14 +1,15 @@
 package spaceguts.graphics.glsl;
 
-import org.lwjgl.opengl.ARBGeometryShader4;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL32;
+import org.lwjgl.opengl.GL40;
 
 public enum ShaderTypes {
 	VERTEX(GL20.GL_VERTEX_SHADER),
 	FRAGMENT(GL20.GL_FRAGMENT_SHADER),
-	GEOMETRY(ARBGeometryShader4.GL_GEOMETRY_SHADER_ARB),
-	TESS_CONTROL(0),
-	TESS_EVALUATION(1);
+	GEOMETRY(GL32.GL_GEOMETRY_SHADER),
+	TESS_CONTROL(GL40.GL_TESS_CONTROL_SHADER),
+	TESS_EVALUATION(GL40.GL_TESS_EVALUATION_SHADER);
 
 	
 	private int glInt;
