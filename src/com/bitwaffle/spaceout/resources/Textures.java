ddaaf98a964cbe0655bc2163617ede0e57a02d05
TranquilMarmot <nate272@gmail.com>
2012-04-12 13:36:02 -0700

    Added missile model

--- src/com/bitwaffle/spaceout/resources/Textures.java
+++ src/com/bitwaffle/spaceout/resources/Textures.java
@@ -27,6 +27,7 @@ public enum Textures {
 	SAUCER("PNG", Paths.SHIPS_PATH.path() + "saucer/saucer.png", false, GL11.GL_NEAREST),
 	WESCOTT("PNG", Paths.SHIPS_PATH.path() + "wescott-8-beta/wescott-8-beta.png", false, GL11.GL_NEAREST),
 	DIAMOND("PNG", Paths.MODEL_PATH.path() + "diamond/diamond.png", false, GL11.GL_NEAREST),
+	MISSILE("PNG", Paths.MODEL_PATH.path() + "missile/missile.png", false, GL11.GL_NEAREST),
 	
 	// Planets
 	EARTH ("JPG", Paths.TEXTURE_PATH.path() + "planets/earth.jpg", true, GL11.GL_LINEAR),
